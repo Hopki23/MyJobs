44cd7217d1a2b2e2ea9bf61435aadffd6b5188e9
Hopki <edis.ersenov23@abv.bg>
2023-07-13 15:25:49 +0300

    Employment service reject method tests added

--- MyJobs/MyJobs.Tests/Services/EmploymentServiceTests.cs
+++ MyJobs/MyJobs.Tests/Services/EmploymentServiceTests.cs
@@ -27,6 +27,8 @@
             context.Database.EnsureCreated();
         }
 
+        //Approve method
+
         [Test]
         public async Task ApproveShouldThrowExceptionForNotExistingJobTest()
         {
@@ -166,7 +168,7 @@
                 IsDeleted = false
             };
 
-             var cv = new CV
+            var cv = new CV
             {
                 Address = "",
                 Education = "",
@@ -264,6 +266,161 @@
             });
 
             Assert.That(exception.Message, Is.EqualTo("The user has already been approved for this job."));
+        }
+
+        //Reject method
+
+        [Test]
+        public async Task RejectShouldThrowExceptionForNotExistingJobTest()
+        {
+            repository = new DbRepository(context);
+            employmentService = new EmploymentService(repository);
+
+            var invalidJobId = 100;
+            var employeeId = 1;
+            var employerId = 1;
+
+            var job = new Job()
+            {
+                Description = "",
+                Offering = "",
+                Requirements = "",
+                Responsibilities = "",
+                Town = "",
+                Title = "",
+                CategoryId = 1,
+                IsDeleted = false
+            };
+
+            await repository.AddAsync(job);
+            await repository.SaveChangesAsync();
+
+            var exception = Assert.ThrowsAsync<ArgumentException>(async () =>
+                await employmentService.Reject(employeeId, employerId, invalidJobId));
+
+            Assert.That(exception.Message, Is.EqualTo("The requested job was not found."));
+        }
+
+        [Test]
+        public async Task RejectShouldThrowExceptionForNotExistingCVTest()
+        {
+            repository = new DbRepository(context);
+            employmentService = new EmploymentService(repository);
+
+            var job = new Job()
+            {
+                Id = 1,
+                Description = "",
+                Offering = "",
+                Requirements = "",
+                Responsibilities = "",
+                Town = "",
+                Title = "",
+                CategoryId = 1,
+                IsDeleted = false
+            };
+
+            var cv = new CV
+            {
+                Address = "",
+                Education = "",
+                Experience = "",
+                Gender = "",
+                PhoneNumber = "",
+                ResumeFile = new byte[0],
+                ResumeFileName = "",
+                Skills = "",
+                Summary = "",
+                Title = ""
+            };
+
+            job.Resumes.Add(cv);
+
+            await repository.AddAsync(job);
+            await repository.SaveChangesAsync();
+
+            var exception = Assert.ThrowsAsync<ArgumentException>(async () =>
+            await this.employmentService.Reject(5, 5, 1));
+
+            Assert.That(exception.Message, Is.EqualTo("The requested resume was not found."));
+        }
+
+        [Test]
+        public async Task RejectShouldWorkCorrectlyTest()
+        {
+            repository = new DbRepository(context);
+            employmentService = new EmploymentService(repository);
+
+            // Create test data
+            var job = new Job
+            {
+                Id = 1,
+                Description = "",
+                Offering = "",
+                Requirements = "",
+                Responsibilities = "",
+                Town = "",
+                Title = "",
+                CategoryId = 1,
+                IsDeleted = false
+            };
+
+            var employee = new Employee
+            {
+                Id = 1,
+                FirstName = "",
+                LastName = "",
+                UserId = ""
+            };
+
+            var employer = new Employer
+            {
+                Id = 1,
+                FirstName = "",
+                LastName = "",
+                UserId = ""
+            };
+
+            var resume = new CV
+            {
+                EmployeeId = 1,
+                Address = "",
+                Education = "",
+                Experience = "",
+                Gender = "",
+                PhoneNumber = "",
+                ResumeFile = new byte[0],
+                ResumeFileName = "",
+                Skills = "",
+                Summary = "",
+                Title = ""
+
+            };
+
+            job.Resumes.Add(resume);
+
+            await repository.AddAsync(job);
+            await repository.AddAsync(employee);
+            await repository.AddAsync(employer);
+            await repository.SaveChangesAsync();
+
+            await employmentService.Reject(1, 1, 1);
+
+            var rejectedJob = await repository.GetByIdAsync<Job>(1);
+            var employeeNotifications = employee.Notifications.ToList();
+            var employerNotifications = employer.Notifications.ToList();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(rejectedJob.Resumes, Is.Empty);
+                Assert.That(employeeNotifications, Has.Count.EqualTo(1));
+                Assert.That(employerNotifications, Has.Count.EqualTo(1));
+            });
+            Assert.Multiple(() =>
+            {
+                Assert.That(employeeNotifications[0].Message, Is.EqualTo("We are sorry, but you are not approved to work with us."));
+                Assert.That(employerNotifications[0].Message, Is.EqualTo("We are sorry, but you are not approved to work with us."));
+            });
         }
     }
 }
