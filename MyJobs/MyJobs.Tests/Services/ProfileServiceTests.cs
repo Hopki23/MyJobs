a5c2d28835f3e87ed955caba89800f31cc425ed6
Hopki <edis.ersenov23@abv.bg>
2023-07-18 20:07:05 +0300

    Added 3 profile service tests

--- MyJobs/MyJobs.Tests/Services/ProfileServiceTests.cs
+++ MyJobs/MyJobs.Tests/Services/ProfileServiceTests.cs
@@ -12,6 +12,7 @@
     using MyJobs.Infrastructure.Models;
     using MyJobs.Core.Models.Profile;
     using MyJobs.Core.Services;
+    using MyJobs.Infrastructure.Data.Models;
 
     [TestFixture]
     public class ProfileServiceTests
@@ -243,6 +244,156 @@
 
             var ex = Assert.ThrowsAsync<ArgumentException>(async () => await profileService.EditProfile(model, employerId, userId));
             Assert.That(ex.Message, Is.EqualTo("Invalid employee"));
+        }
+
+        [Test]
+        public async Task GetReadNotificationsTest()
+        {
+            var UserStoreMock = Mock.Of<IUserStore<ApplicationUser>>();
+            var userMgr = new Mock<UserManager<ApplicationUser>>(UserStoreMock, null, null, null, null, null, null, null, null);
+
+            this.repository = new DbRepository(this.context);
+            var profileService = new ProfileService(this.repository, userMgr.Object);
+
+            var userId = "dasjk";
+
+            var employer = new Employer
+            {
+                Id = 1,
+                FirstName = "",
+                LastName = "",
+                UserId = userId
+            };
+
+            var employee = new Employee
+            {
+                Id = 1,
+                FirstName = "",
+                LastName = "",
+                UserId = userId
+            };
+
+            var notification = new Notification()
+            {
+                Id = 1,
+                EmployeeId = employee.Id,
+                Message = "da",
+                IsRead = true,
+                EmployerId = employer.Id
+            };
+
+            var notification2 = new Notification()
+            {
+                Id = 2,
+                EmployeeId = employee.Id,
+                Message = "daaa",
+                IsRead = true,
+                EmployerId = employer.Id
+            };
+
+            employer.Notifications.Add(notification);
+            employer.Notifications.Add(notification2);
+            employee.Notifications.Add(notification);
+            employee.Notifications.Add(notification2);
+
+            await this.repository.AddAsync(employer);
+            await this.repository.AddAsync(employee);
+            await this.repository.AddAsync(notification);
+            await this.repository.AddAsync(notification2);
+            await this.repository.SaveChangesAsync();
+
+           var notifications = await profileService.GetReadNotifications(userId);
+
+            Assert.That(notifications, Is.Not.Null);
+            Assert.That(notifications.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public async Task GetUnreadNotificationsTest()
+        {
+            var UserStoreMock = Mock.Of<IUserStore<ApplicationUser>>();
+            var userMgr = new Mock<UserManager<ApplicationUser>>(UserStoreMock, null, null, null, null, null, null, null, null);
+
+            this.repository = new DbRepository(this.context);
+            var profileService = new ProfileService(this.repository, userMgr.Object);
+
+            var userId = "dasjk";
+
+            var employer = new Employer
+            {
+                Id = 1,
+                FirstName = "",
+                LastName = "",
+                UserId = userId
+            };
+
+            var employee = new Employee
+            {
+                Id = 1,
+                FirstName = "",
+                LastName = "",
+                UserId = userId
+            };
+
+            var notification = new Notification()
+            {
+                Id = 1,
+                EmployeeId = employee.Id,
+                Message = "da",
+                IsRead = false,
+                EmployerId = employer.Id
+            };
+
+            var notification2 = new Notification()
+            {
+                Id = 2,
+                EmployeeId = employee.Id,
+                Message = "daaa",
+                IsRead = false,
+                EmployerId = employer.Id
+            };
+
+            employer.Notifications.Add(notification);
+            employer.Notifications.Add(notification2);
+            employee.Notifications.Add(notification);
+            employee.Notifications.Add(notification2);
+
+            await this.repository.AddAsync(employer);
+            await this.repository.AddAsync(employee);
+            await this.repository.AddAsync(notification);
+            await this.repository.AddAsync(notification2);
+            await this.repository.SaveChangesAsync();
+
+            var notifications = await profileService.GetUnreadNotifications(userId);
+
+            Assert.That(notifications, Is.Not.Null);
+            Assert.That(notifications.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public async Task MarkNotificationAsRead()
+        {
+            var UserStoreMock = Mock.Of<IUserStore<ApplicationUser>>();
+            var userMgr = new Mock<UserManager<ApplicationUser>>(UserStoreMock, null, null, null, null, null, null, null, null);
+
+            this.repository = new DbRepository(this.context);
+            var profileService = new ProfileService(this.repository, userMgr.Object);
+
+            var notification = new Notification()
+            {
+                Id = 1,
+                Message = "da",
+                IsRead = false,
+            };
+
+            await this.repository.AddAsync(notification);
+            await this.repository.SaveChangesAsync();
+
+            await profileService.MarkNotificationAsRead(1);
+
+            var dbNotification = await this.repository.GetByIdAsync<Notification>(1);
+
+            Assert.That(dbNotification.IsRead, Is.True);
         }
 
         [TearDown]
